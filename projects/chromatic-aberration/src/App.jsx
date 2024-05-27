8c6cb043969f07fa419b77088dbc923e4c2f8961
Markus <marcosbh2002bh@gmail.com>
2024-05-27 15:50:20 +0200

    chore: Add range selector for chromatic aberration project

--- projects/chromatic-aberration/src/App.jsx
+++ projects/chromatic-aberration/src/App.jsx
@@ -2,11 +2,14 @@ import './App.css'
 
 function App() {
   return (
-    <div className="container">
-      <div className="item">Red</div>
-      <div className="item">Green</div>
-      <div className="item">Blue</div>
-    </div>
+    <>
+      <div className="container">
+        <div className="item">Red</div>
+        <div className="item">Green</div>
+        <div className="item">Blue</div>
+      </div>
+      <input className="rangeSelector" type="range" name="space" id="space" />
+    </>
   )
 }
 
